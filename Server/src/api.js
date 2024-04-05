c987f76682af139847b0c39a245e8a017ff3483d
Princepaul Izuogu <123191250+PrincepaulIzuogu@users.noreply.github.com>
2024-04-05 11:17:02 +0200

    Delete Server/src directory

--- Server/src/api.js
+++
@@ -1,9 +0,0 @@
-// App/src/api.js
-
-import axios from 'axios';
-
-const instance = axios.create({
-  baseURL: 'http://localhost:8000', // Update with your backend URL
-});
-
-export default instance;
